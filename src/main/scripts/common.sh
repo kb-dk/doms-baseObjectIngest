6dee1e4c9921be18f1260029efde93e3a7b6d307
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-10-15 15:17:01 +0200

    Basic datamodel works

---
+++ src/main/scripts/common.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
+BASEDIR=$SCRIPT_DIR/..
+
+source $SCRIPT_DIR/setenv.sh
+
+
+mkdir $BASEDIR/logs  >/dev/null 2>&1
+
+
+function batchProcess(){
+  local file=$1
+  output=$($FEDORA_DIR/client/bin/fedora-modify.sh \
+                      $SERVER $USER $PASS \
+                      $file \
+                      $BASEDIR/logs/$(basename $(dirname $file))-$(basename $file .xml).log \
+                      http false 2>&1)
+  echo $output | grep "0 modify directives failed." > /dev/null
+  if [ "$?" == 0 ]; then
+	rm $BASEDIR/logs/$(basename $(dirname $file))-$(basename $file .xml).log  
+  else
+	echo "Failed for $file"
+  fi
+
+}
--- src/main/scripts/createBasicObjects.sh
+++ src/main/scripts/createBasicObjects.sh
@@ -4,12 +4,15 @@
 SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 BASEDIR=$SCRIPT_DIR/..
 
-
 source $SCRIPT_DIR/common.sh
 #
 # Ingest initial objects
 #
 echo "UPDATING base doms objects"
+
+for file in $(ls $BASEDIR/scripts/BasicDatamodel/*/create.xml); do
+    batchProcess $file
+done
 
 for file in $(ls $BASEDIR/scripts/BasicDatamodel/*/setContent.xml); do
     batchProcess $file
@@ -18,6 +21,7 @@ done
 for file in $(ls $BASEDIR/scripts/BasicDatamodel/*/publish.xml); do
     batchProcess $file
 done
+
 
 
 echo "There should be no errors in this result. If there are, something has failed."
