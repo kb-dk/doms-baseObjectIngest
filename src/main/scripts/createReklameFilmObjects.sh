890e583edbd340660778702620c79acb5c533fc4
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-11-12 14:14:42 +0100

    Added the reklamefilm datamodel, along with scripts and whatsnot

---
+++ src/main/scripts/createReklameFilmObjects.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
+BASEDIR=$SCRIPT_DIR/..
+
+
+
+source $SCRIPT_DIR/common.sh
+#
+# Ingest initial objects
+#
+echo "UPDATING base Reklame Film objects"
+
+for file in $(ls $BASEDIR/scripts/ReklameFilmDatamodel/*/create.xml); do
+    batchProcess $file
+    if [ $? -ne 0 ]; then
+        echo "If the object is already there, this will report failures. Just ignore them"
+        echo "If the object is not already there, these errors are important."
+    fi
+done
+
+for file in $(ls $BASEDIR/scripts/ReklameFilmDatamodel/*/setContent.xml); do
+    batchProcess $file
+done
+
+for file in $(ls $BASEDIR/scripts/ReklameFilmDatamodel/*/publish.xml); do
+    batchProcess $file
+done
+
+
+echo "There should be no undeclared errors in this result. If there are, something has failed."
+echo ""
+echo ""
+
+
---
+++ src/main/scripts/updateReklameFilmObjects.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
+BASEDIR=$SCRIPT_DIR/..
+
+source $SCRIPT_DIR/common.sh
+#
+# Ingest initial objects
+#
+echo "UPDATING base doms objects"
+
+for file in $(ls $BASEDIR/scripts/ReklameFilmDatamodel/*/setContent.xml); do
+    batchProcess $file
+done
+
+for file in $(ls $BASEDIR/scripts/ReklameFilmDatamodel/*/publish.xml); do
+    batchProcess $file
+done
+
+
+
+echo "There should be no errors in this result. If there are, something has failed."
+echo ""
+echo ""
+
+
