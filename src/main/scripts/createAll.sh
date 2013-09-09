b0fccea3c838bf5496a2daa73824b988225039f2
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-09-09 14:34:11 +0200

    Added create and update all

---
+++ src/main/scripts/createAll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
+
+$SCRIPT_DIR/createBasicObjects.sh
+$SCRIPT_DIR/createRadioTVObjects.sh
+$SCRIPT_DIR/createReklamefilmObjects.sh
