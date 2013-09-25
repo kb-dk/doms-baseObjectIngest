437d2e3e7386ad2dd969e9d6f4fed584238aef32
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-09-25 11:55:50 +0200

    Include the newspaper datamodel in the scripts

--- src/main/scripts/createAll.sh
+++ src/main/scripts/createAll.sh
@@ -5,3 +5,4 @@ SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 $SCRIPT_DIR/createBasicObjects.sh
 $SCRIPT_DIR/createRadioTVObjects.sh
 $SCRIPT_DIR/createReklamefilmObjects.sh
+$SCRIPT_DIR/createNewspaperObjects.sh
--- src/main/scripts/updateAll.sh
+++ src/main/scripts/updateAll.sh
@@ -5,3 +5,4 @@ SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 $SCRIPT_DIR/updateBasicObjects.sh
 $SCRIPT_DIR/updateRadioTVObjects.sh
 $SCRIPT_DIR/updateReklamefilmObjects.sh
+$SCRIPT_DIR/updateNewspaperObjects.sh
