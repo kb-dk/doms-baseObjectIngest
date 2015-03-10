9605c09255196325a5b80208a308ebb0c4858ced
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-03-10 13:46:03 +0100

    Add a warning if you run the create scripts

--- src/main/scripts/createAll.sh
+++ src/main/scripts/createAll.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ ! "$1" -eq "-q" ]; then
+    echo "This script should only ever be run on fresh installs of DOMS."
+    echo "(You can skip this prompt by supplying -q to you command line)"
+    echo -n "Type \"y\" to continue> "
+    read cont
+    if [ ! "$cont" == "y" ]; then
+        echo "Exiting..."
+        exit 1
+    fi
+fi
+
 SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 
 $SCRIPT_DIR/createBasicObjects.sh
