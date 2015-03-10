716a440c65bd887f7f7680983b7ee1dee45b87cf
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-03-10 13:49:50 +0100

    Remove comments about accepting errors in the result

--- src/main/scripts/createNewspaperObjects.sh
+++ src/main/scripts/createNewspaperObjects.sh
@@ -14,24 +14,15 @@ DIR="$BASEDIR/scripts/NewspaperDatamodel/"
 
 for file in $(ls $DIR/*/create.xml); do
     batchProcess $file
-    if [ $? -ne 0 ]; then
-        echo "If the object is already there, this will report failures. Just ignore them"
-        echo "If the object is not already there, these errors are important."
-    fi
 done
 
 for file in $(ls $DIR/*/setContent.xml); do
     batchProcess $file
 done
-echo "Expect the ContentModel_RoundTrip to report errors"
-
-
 
 for file in $(ls $DIR/*/publish.xml); do
     batchProcess $file
 done
-
-
 
 echo "There should be no undeclared errors in this result. If there are, something has failed."
 echo ""
--- src/main/scripts/createReklamefilmObjects.sh
+++ src/main/scripts/createReklamefilmObjects.sh
@@ -4,8 +4,6 @@
 SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 BASEDIR=$SCRIPT_DIR/..
 
-
-
 source $SCRIPT_DIR/common.sh
 #
 # Ingest initial objects
@@ -14,10 +12,6 @@ echo "Creating base Reklamefilm objects"
 
 for file in $(ls $BASEDIR/scripts/ReklamefilmDatamodel/*/create.xml); do
     batchProcess $file
-    if [ $? -ne 0 ]; then
-        echo "If the object is already there, this will report failures. Just ignore them"
-        echo "If the object is not already there, these errors are important."
-    fi
 done
 
 for file in $(ls $BASEDIR/scripts/ReklamefilmDatamodel/*/setContent.xml); do
@@ -27,7 +21,6 @@ done
 for file in $(ls $BASEDIR/scripts/ReklamefilmDatamodel/*/publish.xml); do
     batchProcess $file
 done
-
 
 echo "There should be no undeclared errors in this result. If there are, something has failed."
 echo ""
