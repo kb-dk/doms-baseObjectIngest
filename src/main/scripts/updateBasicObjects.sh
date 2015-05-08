de838841a998730dc335b8731149b7b21b19cf5d
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-05-08 07:55:20 +0200

    Remove error description

--- src/main/scripts/updateBasicObjects.sh
+++ src/main/scripts/updateBasicObjects.sh
@@ -14,7 +14,6 @@ echo "UPDATING base doms objects"
 for file in $(ls $BASEDIR/scripts/BasicDatamodel/*/setContent.xml); do
     batchProcess $file
 done
-echo "Expect the ContentModel_File to report an error"
 
 for file in $(ls $BASEDIR/scripts/BasicDatamodel/*/publish.xml); do
     batchProcess $file
